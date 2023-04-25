c480ed65e74d52dcdb33b7414891a136684c0a0e
Zexu3366 <Zexu3366.163.com>
2023-04-25 17:15:09 +0800

    忽略idea本地配置，去除了不需要的导入

--- app/src/main/java/com/zexu/serialpicture/repository/entity/SerialPicture.java
+++ app/src/main/java/com/zexu/serialpicture/repository/entity/SerialPicture.java
@@ -1,12 +1,13 @@
 package com.zexu.serialpicture.repository.entity;
 
-//import lombok.Data;
+
 
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "serial_picture")
 public class SerialPicture {
+
     @PrimaryKey(autoGenerate = true)
     public Integer id;
 
