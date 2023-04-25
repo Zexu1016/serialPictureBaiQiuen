6c89c868cb3202156cf07c55e7b78dc29d628537
Zexu3366 <Zexu3366.163.com>
2023-04-25 21:09:52 +0800

    为Service提供了绑定方法

--- app/src/main/java/com/zexu/serialpicture/service/impl/SerialPictureServiceImpl.java
+++ app/src/main/java/com/zexu/serialpicture/service/impl/SerialPictureServiceImpl.java
@@ -1,16 +1,35 @@
 package com.zexu.serialpicture.service.impl;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
 import com.zexu.serialpicture.repository.SerialPictureRepository;
 import com.zexu.serialpicture.repository.entity.SerialPicture;
+import com.zexu.serialpicture.service.CollectService;
 import com.zexu.serialpicture.service.SerialPictureService;
 
 import java.util.List;
 
-public class SerialPictureServiceImpl implements SerialPictureService {
+public class SerialPictureServiceImpl extends Service implements SerialPictureService {
     private SerialPictureRepository serialPictureRepository;
 
     @Override
     public List<SerialPicture> selectAll() {
         return null;
+    }
+
+    class LocalBinder extends Binder {
+        public SerialPictureService getService() {
+            return SerialPictureServiceImpl.this;
+        }
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new LocalBinder();
     }
 }
