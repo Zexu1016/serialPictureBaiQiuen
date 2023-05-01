184692e2fdbfe3520029a597a386ce09f740cfa7
N1YSH <N1YSH227@163.com>
2023-05-01 16:08:33 +0800

    test

---
+++ app/src/main/java/com/zexu/serialpicture/activity/FragmentActivity.java
@@ -0,0 +1,16 @@
+package com.zexu.serialpicture.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zexu.serialpicture.R;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
--- app/src/main/java/com/zexu/serialpicture/activity/MainActivity.java
+++ app/src/main/java/com/zexu/serialpicture/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.zexu.serialpicture.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.zexu.serialpicture.R;
 import com.zexu.serialpicture.service.SerialPictureService;
@@ -10,11 +14,25 @@ import com.zexu.serialpicture.service.SerialPictureService;
 public class MainActivity extends AppCompatActivity {
 
     private SerialPictureService serialPictureService;
+    Button catalogue,label,setting;
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        catalogue = findViewById(R.id.catalogue);
+        label = findViewById(R.id.label);
+        setting = findViewById(R.id.setting);
+
+        catalogue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CatalogueActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //ysh
     }
